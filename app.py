1905d4c1a7c8e27fdfc8fedc21584257deb317b4
XimitiCB <ximiticb@gmail.com>
2022-03-09 02:12:34 -0300

    Test Fail Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Fabian"
+    return "Hello World - Fabian - XXX"
 
 if __name__ == '__main__':
     app.run()
